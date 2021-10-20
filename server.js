a6d33df3aa71096c66831b2f51123d611bd5bd44
Cole Enyart <letchro@gmail.com>
2021-10-19 19:39:03 -0500

    Fixed requires to allow server to load

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/public/assets/js/index.js');
+const note = require('./db/db.json');
 const fs = require("fs");
 
 const PORT = process.env.port || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/api/notes', (req, res) =>
@@ -27,3 +36,7 @@ app.post('/api/notes', (req, res) => {
         res.error('Error in adding note');
     }
 });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
