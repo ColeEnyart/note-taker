0323879bc78d01ea18197f0f51dd56223bea494b
Cole Enyart <letchro@gmail.com>
2021-10-22 19:22:38 -0500

    removed * path

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-app.get('*', (req, res) =>
+/* app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, './public/index.html'))
-);
+); */
 
 app.get('/api/notes', (req, res) => {
     res.status(200).json(globalData);
